7371774b4f36970d4f70eac72046efb2d71642ad
AIM1993 <244018719@qq.com>
2018-10-26 21:16:12 +0800

    修改了前端显示

--- flasksite/frontend/src/index.js
+++ flasksite/frontend/src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello, React!</h1>
+            </div>
+        );
+    }
+}
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
